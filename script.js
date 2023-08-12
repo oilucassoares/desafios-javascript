abd744a8f008c0b2718bd71cb23c0882c5c61a20
Lucas Soares <114198539+oilucassoares@users.noreply.github.com>
2023-08-12 15:22:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function clicar1(){
-    alert('Você clicou no 1º botão!')
-}
-function clicar2(){
-    alert('Você clicou no 2º botão!')
-}
-function clicar3(){
-    alert('Você clicou no 3º botão!')
-}
